95d22454bda4890039a741209ad70e0a84ca57dc
Salaton <nairouasalaton@gmail.com>
2023-08-28 17:01:35 +0300

    initial commit

---
+++ src/main/java/demo/backend/backenddemo/BackenddemoApplication.java
@@ -0,0 +1,13 @@
+package demo.backend.backenddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackenddemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackenddemoApplication.class, args);
+    }
+
+}
