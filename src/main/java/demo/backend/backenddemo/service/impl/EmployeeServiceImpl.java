5539a3c9b815fbec963d87c0aded4394feb90731
Salaton <nairouasalaton@gmail.com>
2023-08-30 14:38:25 +0300

    feat: add api to find patient by ID

--- src/main/java/demo/backend/backenddemo/service/impl/EmployeeServiceImpl.java
+++ src/main/java/demo/backend/backenddemo/service/impl/EmployeeServiceImpl.java
@@ -1,11 +1,13 @@
 package demo.backend.backenddemo.service.impl;
 
+import demo.backend.backenddemo.exception.ResourceNotFoundException;
 import demo.backend.backenddemo.model.Employee;
 import demo.backend.backenddemo.repository.EmployeeRepository;
 import demo.backend.backenddemo.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 //This class implements the employee service interface
 @Service
@@ -25,5 +27,19 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeByID(long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isPresent()) {
+            //.get method returns the content of the optional object
+            return employee.get();
+        } else {
+            throw new ResourceNotFoundException("Employee", "Id", id);
+        }
+
+        // Can use lambda expressions
+        // return employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee", "Id", id));
     }
 }
